39bc7cd157f042b14a8b41059fc50a86083ba814
Muhammad Ikrash <mikrash62@gmail.com>
2026-06-25 23:15:11 +0500

    Note App Completed

---
+++ Note_app/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { RiCloseFill } from "@remixicon/react";
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [textValue, setTextValue] = useState("");
+  const [task, setTask] = useState([]);
+  const onFormSub = (e) => {
+    e.preventDefault();
+    const copyTask = [...task];
+    copyTask.push({ inputValue, textValue });
+    setTask(copyTask);
+    setTextValue("");
+    setInputValue("");
+  };
+
+  const getInputVal = (e) => {
+    setInputValue(e);
+  };
+  const getTextVal = (e) => {
+    setTextValue(e);
+  };
+  const delNote = (imd) => {
+    const copyTask = [...task];
+    copyTask.splice(imd, 1);
+    setTask(copyTask);
+  };
+  return (
+    <div className="bg-black  lg:flex text-white ">
+      <form
+        onSubmit={(e) => {
+          onFormSub(e);
+        }}
+        className="flex lg:w-1/2 items-start flex-col gap-4 p-10 "
+      >
+        <h1 className="font-bold text-4xl">Your Notes</h1>
+        <input
+          type="text"
+          className="px-5 py-2 border-2 font-medium w-full rounded outline-none"
+          placeholder="Enter Note Title"
+          onChange={(e) => {
+            getInputVal(e.target.value);
+          }}
+          value={inputValue}
+        />
+        <textarea
+          type="text"
+          className="px-5 py-2 h-25 border-2 font-medium w-full rounded outline-none"
+          placeholder="Write Detail"
+          onChange={(e) => {
+            getTextVal(e.target.value);
+          }}
+          value={textValue}
+        />
+        <button className="bg-white active:bg-gray-200 font-medium text-black w-full px-5 py-2 h-20 rounded outline-none">
+          Add Note
+        </button>
+      </form>
+      <div className=" lg:w-1/2 lg:border-l-2 p-10 h-screen overflow-auto scrollbar-none">
+        <h1 className="font-bold text-4xl">Recent Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5">
+          {task.map((elm, imd) => {
+            return (
+              <div
+                key={imd}
+                id={imd}
+                className="h-52 w-40 bg-[url('https://img.magnific.com/free-vector/leafy-patterned-note-background_53876-97734.jpg')] bg-cover rounded text-black px-4 py-6 "
+              >
+                <h2
+                  onClick={() => {
+                    delNote(imd);
+                  }}
+                  className="relative  left-28 bottom-2"
+                >
+                  <RiCloseFill
+                    size={20}
+                    className="bg-red-600 rounded-full cursor-pointer active:bg-red-500 w-5 h-5"
+                  />
+                </h2>
+                <h3 className="leading-tight font-bold text-xl ">
+                  {elm.inputValue}
+                </h3>
+                <p className="mt-2 leading-tight font-medium text-gray-500 ">
+                  {elm.textValue}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
