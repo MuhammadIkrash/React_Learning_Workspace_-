12cb4a7ad9ab05e696a2e039df706f0083b4c0a8
Muhammad Ikrash <mikrash62@gmail.com>
2026-06-25 14:38:49 +0500

    Practice Form Handling

--- FormHandlingInReact/src/App.jsx
+++ FormHandlingInReact/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [inpVal, setInputVal] = useState("");
+  let formSub = (e) => {
+    e.preventDefault();
+  };
+  let SetVal = (e) => {
+   setInputVal(e)
+  };
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <form
+        onSubmit={(e) => {
+          formSub(e);
+          console.log('form Submit >>>>>', inpVal);
+          
+        }}
+      >
+        <input
+          onChange={(e) => {
+            SetVal(e.target.value)
+          }}
+          value={inpVal}
+          type="text"
+          placeholder="Enter Name"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
